a29874f2448c7368f435b96f718b45b72f1ca84e
ryohei-takasugi <63960937+ryohei-takasugi@users.noreply.github.com>
2021-10-25 17:20:27 +0900

    ユーザー認証の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # validations
+  validates :nickname, presence: true, length: { maximum: 6, message: "ニックネームは６文字以下です。" }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "experiences#index"
   resources :experiences, only: [:index]
+  resources :users, only: [:show]
 end
