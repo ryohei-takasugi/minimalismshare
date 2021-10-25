c6bb5a663eb7643c907bd57fe751681dcb2278e8
ryohei-takasugi <63960937+ryohei-takasugi@users.noreply.github.com>
2021-10-25 15:34:18 +0900

    ルートページの仮実装（アカウント新規作成機能を作るため）

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configre_permitted_paramter, if: :devise_controller?
+
+  private
+  def configre_permitted_paramter
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
 end
