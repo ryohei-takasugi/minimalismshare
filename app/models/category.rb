075f582c7297c12916502ee9dbb074b654f7e077
ryohei-takasugi <63960937+ryohei-takasugi@users.noreply.github.com>
2021-10-29 11:28:37 +0900

    カテゴリの文言変更

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,9 @@
 class Category < ActiveHash::Base
   self.data = [
     { id: 0, name: '--' },
-    { id: 1, name: '成功' },
-    { id: 2, name: '失敗' }
+    { id: 1, name: '成功体験' },
+    { id: 2, name: '失敗体験' },
+    { id: 3, name: 'やってみた' }
   ]
 
   include ActiveHash::Associations
