ec40df02bb555e1698682c8ed9f046f21137f731
ryohei-takasugi <63960937+ryohei-takasugi@users.noreply.github.com>
2021-10-29 11:14:59 +0900

    経験の投稿に関わるルーティングの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "experiences#index"
-  resources :experiences, only: [:index]
+  resources :experiences, only: [:index, :new, :create] do
+    collection do
+      get 'search'
+    end
+  end
   resources :users, only: [:show]
 end
