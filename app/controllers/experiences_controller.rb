9787e6e58aa9f571282134c776eb1e1cd8d9b3c9
ryohei-takasugi <63960937+ryohei-takasugi@users.noreply.github.com>
2021-10-29 11:16:49 +0900

    ExperiencesControllerの実装

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -1,2 +1,33 @@
 class ExperiencesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
+  def index
+    @experiences = Experience.all.order(update_at: :desc).includes(:user)
+  end
+  
+  def new
+    @experience = ExperienceTag.new
+  end
+
+  def create
+    @experience = ExperienceTag.new(experience_params)
+    if @experience.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def search
+    return nil if params[:keyword] == ""
+    tag = Tag.where(['name LIKE ?', "%#{params[:keyword]}%"] )
+    render json:{ keyword: tag }
+  end
+  
+  private
+
+    def experience_params
+      params.require(:experience).permit(:title, :tags, :content, :category_id, :period_id).merge(user_id: current_user.id)
+    end
+
 end
