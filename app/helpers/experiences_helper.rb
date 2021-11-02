aa8e2a98dc6bed5ab7ebd1b37d91760a5ef23cc1
ryohei-takasugi <63960937+ryohei-takasugi@users.noreply.github.com>
2021-11-02 11:41:39 +0900

    Viewでモデルへアクセスしないようにhelperに移動

--- app/helpers/experiences_helper.rb
+++ app/helpers/experiences_helper.rb
@@ -1,2 +1,24 @@
 module ExperiencesHelper
+  def tags_join(tags)
+    # <% experience.tags.each do |tag| %>
+    #   <p class="tag"><%= tag.name %></p>
+    # <% end %>
+    array = Array.new
+    tags.each do |tag|
+      array << tag.name
+    end
+    array.join('、')
+  end
+
+  def set_tag
+    Tag.all
+  end
+
+  def set_category
+    Category.all
+  end
+
+  def set_period
+    Period.all
+  end
 end
