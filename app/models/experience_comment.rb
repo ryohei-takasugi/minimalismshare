6483cdf7e3aa070bd88375e262255d0ac9df9ca5
ryohei-takasugi <63960937+ryohei-takasugi@users.noreply.github.com>
2021-11-03 12:00:22 +0900

    経験の投稿に紐づくコメントモデルの生成

---
+++ app/models/experience_comment.rb
@@ -0,0 +1,2 @@
+class ExperienceComment < ApplicationRecord
+end
---
+++ spec/models/experience_comment_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe ExperienceComment, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
