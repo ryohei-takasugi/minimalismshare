99c011cf6c79a2e47d189e4d5eb267c5c4e42d18
ryohei-takasugi <63960937+ryohei-takasugi@users.noreply.github.com>
2021-11-08 21:43:03 +0900

    モデルファイルのフォルダ分け

--- app/models/user/user.rb
+++ app/models/user/user.rb
@@ -11,7 +11,10 @@ class User < ApplicationRecord
   belongs_to :children
   belongs_to :clean_status
   # association DataBase
-  # なし
+  has_many :experiences
+  has_many :experience_likes
+  has_many :experience_comments
+  has_many :notices
   # validations
   validates :nickname, presence: true, length: { maximum: 6, message: 'ニックネームは６文字以下です。' }
 end
