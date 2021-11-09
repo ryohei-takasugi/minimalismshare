2b7be8c5849e1417ffe9920221a822d387306de3
ryohei-takasugi <63960937+ryohei-takasugi@users.noreply.github.com>
2021-11-09 18:20:14 +0900

    モデルの単体テスト実装

--- spec/factories/notices.rb
+++ spec/factories/notices.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :notice do
-    
+    message     { Faker::Lorem.sentence }
+    url         { Faker::Internet.url(host: '127.0.0.1:3000') }
+    read        { false }
+    association :user
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,11 +4,12 @@ FactoryBot.define do
     password              { Faker::Lorem.characters(number: 6, min_alpha: 1, min_numeric: 1) }
     password_confirmation { password }
     nickname              { Faker::Lorem.characters(number: 6) }
-    dream                 { Faker::Number.within(range: 0..3) }
-    region_id             { Faker::Number.within(range: 0..3) }
-    climate_id            { Faker::Number.within(range: 0..3) }
+    dream                 { Faker::Lorem.characters }
+    high_id               { Faker::Number.within(range: 0..9) }
+    low_id                { Faker::Number.within(range: 0..9) }
     housemate_id          { Faker::Number.within(range: 0..3) }
-    children_id           { Faker::Number.within(range: 0..3) }
+    hobby_id              { Faker::Number.within(range: 0..3) }
     clean_status_id       { Faker::Number.within(range: 0..3) }
+    range_with_store_id   { Faker::Number.within(range: 0..4) }
   end
 end
