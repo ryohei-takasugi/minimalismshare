b267912ed90f54b5750bc5fdd56e6381861c8e2f
ryohei-takasugi <63960937+ryohei-takasugi@users.noreply.github.com>
2021-11-24 19:11:05 +0900

    controllerの一部処理をscopeに移動

--- app/models/user/notice.rb
+++ app/models/user/notice.rb
@@ -4,4 +4,7 @@ class Notice < ApplicationRecord
   # validations
   validates :message, presence: true
   validates :url,     presence: true
+  # scope
+  scope :recent, -> (user_id) { where(user_id: user_id).order(id: :desc).limit(10) }
+  scope :recent_100, -> (user_id) { where(user_id: user_id).order(id: :desc).limit(100) }
 end
