c14688117467e62ba210568d9a6082aa7e97e0c9
ryohei-takasugi <63960937+ryohei-takasugi@users.noreply.github.com>
2021-12-21 17:43:01 +0900

    テストケースの追加

--- spec/requests/experience_likes_spec.rb
+++ spec/requests/experience_likes_spec.rb
@@ -6,11 +6,27 @@ RSpec.describe 'ExperienceLikes', type: :request do
     before do
       @user1 = FactoryBot.create(:user)
       @user2 = FactoryBot.create(:user)
-      @experience_tag = create_experience_tag(user_model: @user1)
-      sign_in @user1
+      @experience_tag = create_experience_tag(user_model: @user2)
     end
     context 'POST   /experiences/:experience_id/experience_likes' do
+      it 'returns http error 401' do
+        like_params = { experiences_like: { like: true }, experience_id: @experience_tag.id, user_id: @user1.id }
+        expect do
+          post "/experiences/#{@experience_tag.id}/experience_likes", params: like_params, headers: nil
+        end.to change { ExperienceLike.count }.by(0)
+        expect(response).to have_http_status 401
+      end
+      it 'redirect experiences' do
+        sign_in @user2
+        like_params = { experiences_like: { like: true }, experience_id: @experience_tag.id, user_id: @user2.id }
+        expect do
+          post "/experiences/#{@experience_tag.id}/experience_likes", params: like_params, headers: auth_login
+        end.to change { ExperienceLike.count }.by(0)
+        expect(response).to have_http_status 302
+        expect(response.redirect_url.match("http://#{response.request.env["SERVER_NAME"]}/(.*)")[1]).to eq("experiences")
+      end
       it 'returns http success(like)' do
+        sign_in @user1
         like_params = { experiences_like: { like: true }, experience_id: @experience_tag.id, user_id: @user1.id }
         expect do
           post "/experiences/#{@experience_tag.id}/experience_likes", params: like_params, headers: auth_login
@@ -19,6 +35,7 @@ RSpec.describe 'ExperienceLikes', type: :request do
         expect(response.redirect_url.match("http://#{response.request.env["SERVER_NAME"]}/(.*)")[1]).to eq("experiences/#{@experience_tag.id}")
       end
       it 'returns http success(imitate)' do
+        sign_in @user1
         like_params = { experiences_like: { imitate: true }, experience_id: @experience_tag.id, user_id: @user1.id }
         expect do
           post "/experiences/#{@experience_tag.id}/experience_likes", params: like_params, headers: auth_login
@@ -29,9 +46,28 @@ RSpec.describe 'ExperienceLikes', type: :request do
     end
     context 'PATCH  /experiences/:experience_id/experience_likes/:id' do
       before do
+        sign_in @user1
         like_params = { experiences_like: { like: true, imitate: true }, experience_id: @experience_tag.id, user_id: @user1.id }
         post "/experiences/#{@experience_tag.id}/experience_likes", params: like_params, headers: auth_login
         @experience_likes = ExperienceLike.find_by(experience_id: @experience_tag.id, user_id: @user1.id)
+      end
+      it 'returns http error 401' do
+        like_params = { experiences_like: { like: true }, experience_id: @experience_tag.id, user_id: @user1.id }
+        sign_out @user1
+        expect do
+          patch "/experiences/#{@experience_tag.id}/experience_likes/#{@experience_likes.id}", params: like_params, headers: nil
+        end.to change { ExperienceLike.count }.by(0)
+        expect(response).to have_http_status 401
+      end
+      it 'redirect experiences' do
+        sign_out @user1
+        sign_in @user2
+        like_params = { experiences_like: { like: true }, experience_id: @experience_tag.id, user_id: @user2.id }
+        expect do
+          patch "/experiences/#{@experience_tag.id}/experience_likes/#{@experience_likes.id}", params: like_params, headers: auth_login
+        end.to change { ExperienceLike.count }.by(0)
+        expect(response).to have_http_status 302
+        expect(response.redirect_url.match("http://#{response.request.env["SERVER_NAME"]}/(.*)")[1]).to eq("experiences")
       end
       it 'returns http success(like)' do
         like_params = { experiences_like: { like: false }, experience_id: @experience_tag.id, user_id: @user1.id }
@@ -52,9 +88,28 @@ RSpec.describe 'ExperienceLikes', type: :request do
     end
     context 'PUT    /experiences/:experience_id/experience_likes/:id' do
       before do
+        sign_in @user1
         like_params = { experiences_like: { like: true, imitate: true }, experience_id: @experience_tag.id, user_id: @user1.id }
         post "/experiences/#{@experience_tag.id}/experience_likes", params: like_params, headers: auth_login
         @experience_likes = ExperienceLike.find_by(experience_id: @experience_tag.id, user_id: @user1.id)
+      end
+      it 'returns http error 401' do
+        like_params = { experiences_like: { like: true }, experience_id: @experience_tag.id, user_id: @user1.id }
+        sign_out @user1
+        expect do
+          put "/experiences/#{@experience_tag.id}/experience_likes/#{@experience_likes.id}", params: like_params, headers: nil
+        end.to change { ExperienceLike.count }.by(0)
+        expect(response).to have_http_status 401
+      end
+      it 'redirect experiences' do
+        sign_out @user1
+        sign_in @user2
+        like_params = { experiences_like: { like: true }, experience_id: @experience_tag.id, user_id: @user2.id }
+        expect do
+          put "/experiences/#{@experience_tag.id}/experience_likes/#{@experience_likes.id}", params: like_params, headers: auth_login
+        end.to change { ExperienceLike.count }.by(0)
+        expect(response).to have_http_status 302
+        expect(response.redirect_url.match("http://#{response.request.env["SERVER_NAME"]}/(.*)")[1]).to eq("experiences")
       end
       it 'returns http success(like)' do
         like_params = { experiences_like: { like: false }, experience_id: @experience_tag.id, user_id: @user1.id }
