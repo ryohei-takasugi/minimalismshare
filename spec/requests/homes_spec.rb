0b11802a1ddd245402ee4221e38707b6a1f621eb
ryohei-takasugi <63960937+ryohei-takasugi@users.noreply.github.com>
2021-12-21 13:48:08 +0900

    homesのRequestSpecを実装

--- spec/requests/homes_spec.rb
+++ spec/requests/homes_spec.rb
@@ -1,9 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'Homes', type: :request do
-  describe 'GET /index' do
+  describe 'GET    /' do
+    it 'returns http error' do
+      get root_path, params: nil, headers: nil
+      expect(response).to have_http_status(401)
+    end
     it 'returns http success' do
-      get '/homes/index'
+      get root_path, params: nil, headers: auth_login
       expect(response).to have_http_status(:success)
     end
   end
