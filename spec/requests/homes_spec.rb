94e1f01708784357d593ab265640d1fadd56f867
ryohei-takasugi <63960937+ryohei-takasugi@users.noreply.github.com>
2021-12-21 16:34:47 +0900

    記述の統一

--- spec/requests/homes_spec.rb
+++ spec/requests/homes_spec.rb
@@ -1,14 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe 'Homes', type: :request do
-  describe 'GET    /' do
-    it 'returns http error' do
-      get root_path, params: nil, headers: nil
-      expect(response).to have_http_status(401)
+  describe 'GET' do
+    context 'GET    /' do
+      it 'returns http error' do
+        get '/', params: nil, headers: nil
+        expect(response).to have_http_status 401
+      end
+      it 'returns http success' do
+        get '/', params: nil, headers: auth_login
+        expect(response).to have_http_status 200
+      end
     end
-    it 'returns http success' do
-      get root_path, params: nil, headers: auth_login
-      expect(response).to have_http_status(:success)
+    context 'GET    /homes' do
+      it 'returns http error' do
+        get '/homes', params: nil, headers: nil
+        expect(response).to have_http_status 401
+      end
+      it 'returns http success' do
+        get '/homes', params: nil, headers: auth_login
+        expect(response).to have_http_status 200
+      end
+    end
+    context 'GET    /homes/about' do
+      it 'returns http error' do
+        get '/homes/about', params: nil, headers: nil
+        expect(response).to have_http_status 401
+      end
+      it 'returns http success' do
+        get '/homes/about', params: nil, headers: auth_login
+        expect(response).to have_http_status 200
+      end
     end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,25 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /show' do
-    before do
-      @user1 = FactoryBot.create(:user)
-      @user2 = FactoryBot.create(:user)
-    end
-    it 'returns http error 401' do
-      sign_in @user1
-      get "/users/#{@user1.id}", params: nil, headers: nil
-      expect(response).to have_http_status 401
-    end
-    it 'redirect_to experiences' do
-      sign_in @user2
-      get "/users/#{@user1.id}", params: nil, headers: auth_login
-      expect(response).to have_http_status 302
-    end
-    it 'returns http success' do
-      sign_in @user1
-      get "/users/#{@user1.id}", params: nil, headers: auth_login
-      expect(response).to have_http_status 200
+  describe 'GET' do
+    context 'GET    /show' do
+      before do
+        @user1 = FactoryBot.create(:user)
+        @user2 = FactoryBot.create(:user)
+      end
+      it 'returns http error 401' do
+        sign_in @user1
+        get "/users/#{@user1.id}", params: nil, headers: nil
+        expect(response).to have_http_status 401
+      end
+      it 'redirect_to experiences' do
+        sign_in @user2
+        get "/users/#{@user1.id}", params: nil, headers: auth_login
+        expect(response).to have_http_status 302
+      end
+      it 'returns http success' do
+        sign_in @user1
+        get "/users/#{@user1.id}", params: nil, headers: auth_login
+        expect(response).to have_http_status 200
+      end
     end
   end
 end
